44f646875b753244b4fc2cb1bf1eae8de7055e64
waledalandable <156304076+waledalandable@users.noreply.github.com>
2026-04-23 02:56:43 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-import os
+BOT_TOKEN = "7787809968:AAHpKbErc_GmNfFaK0uLmlD__4taeNojSLQ"
 
-BOT_TOKEN = os.getenv("7787809968:AAHpKbErc_GmNfFaK0uLmlD__4taeNojSLQ ")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "7496673554"))
+ADMIN_ID = 7496673554
 
 PLANS = {
     "basic": {"price": "10$", "limit": 100},
