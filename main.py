8bda5e9c24c8617935d01b9f79cd00d98b873bf7
waledalandable <156304076+waledalandable@users.noreply.github.com>
2026-04-23 02:26:52 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import telebot
 from config import BOT_TOKEN
 from handlers import register
-from keep_alive import keep_alive
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 register(bot)
-
-keep_alive()
 
 print("Bot running...")
 bot.infinity_polling()
